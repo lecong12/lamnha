3d960bb2e104b846838a75e239494a6fe3661beb
Lê Công <lecong12@gmail.com>
2026-04-15 09:33:13 +0700

    cập nhật

--- src/components/sheetsAPI.js
+++ src/components/sheetsAPI.js
@@ -1,5 +1,5 @@
 // AppSheet API Configuration
-import { toInputString, toDisplayString } from './dateUtils';
+import { toInputString, toDisplayString } from '../utils/dateUtils';
 const APPSHEET_ACCESS_KEY = process.env.REACT_APP_APPSHEET_ACCESS_KEY;
 const TABLE_GIAODICH_ENV = process.env.REACT_APP_APPSHEET_TABLE_GIAODICH || "GiaoDich";
 // Helper để chuẩn hóa key từ AppSheet về chuẩn code (ngay, noiDung, id...)
